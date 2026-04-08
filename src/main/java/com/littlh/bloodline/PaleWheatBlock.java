5e4228123b2b30eefd4c2872d54d7606b5f22f23
littlh <117704805+littlhMW@users.noreply.github.com>
2026-04-08 08:13:31 +0000

    正常生成小麦

--- src/main/java/com/littlh/bloodline/PaleWheatBlock.java
+++ src/main/java/com/littlh/bloodline/PaleWheatBlock.java
@@ -1,17 +1,18 @@
 package com.littlh.bloodline;
 
-import net.minecraft.world.level.block.CropBlock;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.level.material.PushReaction;
 
-public class PaleWheatBlock extends CropBlock {
+public class PaleWheatBlock extends Block {
     public PaleWheatBlock() {
         super(BlockBehaviour.Properties.of()
                 .mapColor(MapColor.COLOR_GRAY)
                 .noCollission()
-                .randomTicks()
                 .instabreak()
-                .sound(SoundType.CROP));
+                .sound(SoundType.GRASS)
+                .pushReaction(PushReaction.DESTROY));
     }
 }
